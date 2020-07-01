3295a41af8bd3b2c74f40a1c2c7ff470e2529f26
spanki22052 <useful22052@gmail.com>
2020-07-01 16:36:02 +0300

    reviews block

--- protein/src/components/MainPage/index.jsx
+++ protein/src/components/MainPage/index.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./main.scss";
 
 const MainPage = () => {
+
+    const company = "DagProtein";
+
   return (
     <div className="main-page">
       <div className="header">
         <div className="navbar-block">
-          <h1>DagProtein</h1>
+          <h1>{company}</h1>
           <div className="pbadge">
             <p>качество</p>
           </div>
@@ -34,7 +37,7 @@ const MainPage = () => {
         />
         <div className="text-block">
           <h2>
-            Мы распологаем одной из лучших протеиновых продукция в Махачкале.
+            Мы распологаем одной из лучших протеиновых продукций в Махачкале.
           </h2>
           <button> Перейти к товарам </button>
         </div>
@@ -58,6 +61,24 @@ const MainPage = () => {
               <img src="/images/protein1.png" alt="proteinproduct" />
               <button>Подробнее</button>
             </div>
+          </div>
+        </div>
+
+        <div className="reviews-block">
+          <div className="review-block">
+            <img src="/images/magomed.jpg" alt="manphoto" />
+            <h1>Магомед, 21 год</h1>
+            <p>Пользуюсь продукцией {company} около года. Всем доволен.</p>
+          </div>
+          <div className="review-block">
+            <img src="/images/man.jpg" alt="manphoto" />
+            <h1>Супиян, 24 года</h1>
+            <p>Тренируюсь два года, с начала тренировки закупаюсь в этом магазине.</p>
+          </div>
+          <div className="review-block">
+            <img src="/images/dsh.jpg" alt="manphoto" />
+            <h1>Амир, 18 лет</h1>
+            <p>Недавно начал тренироваться. В {company} все дешево и качественно.</p>
           </div>
         </div>
       </div>
