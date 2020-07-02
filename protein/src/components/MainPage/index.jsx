8e6665076085d06e81f04cdd6783b1ac1b88900e
spanki22052 <useful22052@gmail.com>
2020-07-02 21:22:48 +0300

    finished frontend

--- protein/src/components/MainPage/index.jsx
+++ protein/src/components/MainPage/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./main.scss";
 
 const MainPage = () => {
-
-    const company = "DagProtein";
+  const company = "Dagestan";
+  const bodyWidth = window.innerWidth;
 
   return (
     <div className="main-page">
@@ -25,6 +25,18 @@ const MainPage = () => {
             </li>
           </ul>
         </div>
+
+        <img
+          className="small-green-box"
+          src="./svg/small-green-box.svg"
+          alt="greenround"
+        />
+        <img
+          className="small-proteins"
+          src="/images/small-proteins.png"
+          alt="proteinsadvertisement"
+        />
+
         <img
           className="green-box"
           src="./svg/green-header-round.svg"
@@ -46,7 +58,13 @@ const MainPage = () => {
       <div className="main">
         <div
           className="products-block"
-          style={{ background: 'url("/svg/green-products-block.svg")' }}
+          style={
+            bodyWidth > 900
+              ? { backgroundImage: 'url("/svg/green-products-block.svg")' }
+              : {
+                  backgroundImage: 'url("/svg/green-small-products-block.svg")',
+                }
+          }
         >
           <div className="products-padding">
             <div className="product-block">
@@ -73,13 +91,55 @@ const MainPage = () => {
           <div className="review-block">
             <img src="/images/man.jpg" alt="manphoto" />
             <h1>Супиян, 24 года</h1>
-            <p>Тренируюсь два года, с начала тренировки закупаюсь в этом магазине.</p>
+            <p>
+              Тренируюсь два года, с начала тренировки закупаюсь в этом
+              магазине.
+            </p>
           </div>
           <div className="review-block">
             <img src="/images/dsh.jpg" alt="manphoto" />
             <h1>Амир, 18 лет</h1>
-            <p>Недавно начал тренироваться. В {company} все дешево и качественно.</p>
+            <p>
+              Недавно начал тренироваться. В {company} все дешево и качественно.
+            </p>
           </div>
+        </div>
+
+        <div className="train-with-us-block">
+          <div
+            className="left-block"
+            style={
+              bodyWidth > 900
+                ? { backgroundImage: 'url("/svg/muscle-man.svg")' }
+                : {
+                    backgroundImage: 'url("/svg/small-muscle-man.svg")',
+                  }
+            }
+          >
+            <div className="left-padding">
+              <img src="/svg/flexing-man.svg" alt="muscleman" />
+              <div className="white-text-block">
+                <h1>Заказывайте протеины в {company}.ml</h1>
+                <button>Перейти в магазин</button>
+              </div>
+            </div>
+          </div>
+          <div
+            className="right-block"
+            style={{ background: "url('/svg/train-with-us.svg')" }}
+          >
+            <div className="right-padding">
+              <img src="/svg/yellow-muscle-man.svg" alt="yellow" />
+              <h1>Заряжайтесь энергией вместе с {company}.</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <p>Spanki prod.</p>
+        <div className="icons-block">
+          <img className="vk-icon" src="/svg/vk.svg" alt="vk" />
+          <img className="git-icon" src="/svg/git.svg" alt="git" />
         </div>
       </div>
     </div>
