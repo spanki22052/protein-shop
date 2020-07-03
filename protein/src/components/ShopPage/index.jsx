7a713a4b8c0ce3836c4399cb195a8a4566ab1b89
spanki22052 <useful22052@gmail.com>
2020-07-03 10:58:08 +0300

    Create index.jsx

---
+++ protein/src/components/ShopPage/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShopPage extends Component {
+    render() {
+        return(
+            <div className="shop-page">
+                
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
