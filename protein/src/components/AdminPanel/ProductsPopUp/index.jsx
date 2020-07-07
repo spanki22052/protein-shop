7ad9d5c26e49c345c7f213030945f410191f8461
spanki22052 <useful22052@gmail.com>
2020-07-07 15:57:09 +0300

    finish problem

--- protein/src/components/AdminPanel/ProductsPopUp/index.jsx
+++ protein/src/components/AdminPanel/ProductsPopUp/index.jsx
@@ -25,40 +25,42 @@ class ProductsPopUp extends Component {
         className="products-popup"
       >
         <div className="products-popup__padding">
-          {this.getKeys(this.props.currentPopupProduct).map(
-            (element, index) => {
-              return (
-                element !== "counter" && (
-                  <div key={index} className="product-block">
-                    <div className="image-side">
-                      <img
-                        src={this.props.currentPopupProduct[element].image}
-                        alt="productImage"
-                      />
+          <div className="products-block">
+            {this.getKeys(this.props.currentPopupProduct).map(
+              (element, index) => {
+                return (
+                  element !== "counter" && (
+                    <div key={index} className="product-block">
+                      <div className="image-side">
+                        <img
+                          src={this.props.currentPopupProduct[element].image}
+                          alt="productImage"
+                        />
+                      </div>
+                      <div className="text-side">
+                        <p>{this.props.currentPopupProduct[element].title}</p>
+                        <p>
+                          Цена за шт.{" "}
+                          {this.props.currentPopupProduct[element].price} руб
+                        </p>
+                        <p>
+                          Количество:{" "}
+                          {this.props.currentPopupProduct[element].amount}
+                        </p>
+                        <p>
+                          Всего:
+                          {this.props.currentPopupProduct[element].amount *
+                            this.props.currentPopupProduct[element].price}
+                          р
+                        </p>
+                      </div>
+                      <div className="line"></div>
                     </div>
-                    <div className="text-side">
-                      <p>{this.props.currentPopupProduct[element].title}</p>
-                      <p>
-                        Цена за шт.{" "}
-                        {this.props.currentPopupProduct[element].price} руб
-                      </p>
-                      <p>
-                        Количество:{" "}
-                        {this.props.currentPopupProduct[element].amount}
-                      </p>
-                      <p>
-                        Всего:
-                        {this.props.currentPopupProduct[element].amount *
-                          this.props.currentPopupProduct[element].price}
-                        р
-                      </p>
-                    </div>
-                    <div className="line"></div>
-                  </div>
-                )
-              );
-            }
-          )}
+                  )
+                );
+              }
+            )}
+          </div>
 
           <img
             className="closeIcon"
