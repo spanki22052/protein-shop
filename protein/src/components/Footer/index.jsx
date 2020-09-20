5880be1fe8b3ab2a15cd7e1532272956f1ebded8
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-09-20 16:54:53 +0300

    add some scss

--- protein/src/components/Footer/index.jsx
+++ protein/src/components/Footer/index.jsx
@@ -3,35 +3,36 @@ import "./footer.scss";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <div className="footer-element">
-      <div className="first-block">
-        <h1>BIOMART</h1>
-      </div>
+	return (
+		<div className="footer-element">
+			<div className="first-block">
+				<h1>BIOMART</h1>
+			</div>
 
-      <div className="second-block">
-        <h1>Biomart</h1>
-        <Link className="go-to" to="/about">
-          О нас
+			<div className="second-block">
+				<h1>Biomart</h1>
+				<Link className="go-to" to="/about">
+					О нас
         </Link>
-        <a className="go-to" href="http://instagram.com">
-          Наш инстаграм
+				<a className="go-to" href="http://instagram.com">
+					Наш инстаграм
         </a>
-        <Link className="go-to" to="/shop">
-          Перейти в магазин
+				<Link className="go-to" to="/shop">
+					Перейти в магазин
         </Link>
-      </div>
-      <div className="third-block">
-        <h1>Связаться с нами</h1>
+			</div>
+			<div className="third-block">
+				<h1>Связаться с нами</h1>
 
-        <div className="icons-block">
-          <img className="instagram" src="/svg/instagram.svg" alt="inst" />
-          <img className="vk" src="/svg/vk.svg" alt="vk" />
-          <img className="phone" src="/svg/phone.svg" alt="phone" />
-        </div>
-      </div>
-    </div>
-  );
+				<div className="icons-block">
+					<img className="instagram" src="/svg/instagram.svg" alt="inst" />
+					<img className="vk" src="/svg/vk.svg" alt="vk" />
+					<img className="phone" src="/svg/phone.svg" alt="phone" />
+				</div>
+				<div className='phoneNum'>+7999-525-34-52</div>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
--- protein/src/components/NewMainPage/index.jsx
+++ protein/src/components/NewMainPage/index.jsx
@@ -2,153 +2,164 @@ import React, { useEffect, useState } from "react";
 import "./new.scss";
 import Footer from "../Footer";
 import { Link } from "react-router-dom";
+import HorizontalScroll from 'react-scroll-horizontal'
 
 const firebase = require("firebase");
 
 function NewMainPage() {
-  const [categories, setCategories] = useState([]);
-  const [products, setProducts] = useState({});
-  var counter = 0;
+	const [categories, setCategories] = useState([]);
+	const [products, setProducts] = useState({});
+	var counter = 0;
 
-  const hexObject = {
-    "В НАЛИЧИИ": "white",
-    "СКИДКА 10%": "#EC2929",
-    ПРЕДЗАКАЗ: "#265ECB",
-    "НЕТ В НАЛИЧИИ": "#BCBCBC",
-  };
+	const hexObject = {
+		"В НАЛИЧИИ": "white",
+		"СКИДКА 10%": "#EC2929",
+		ПРЕДЗАКАЗ: "#265ECB",
+		"НЕТ В НАЛИЧИИ": "#BCBCBC",
+	};
 
-  useEffect(() => {
-    firebase
-      .firestore()
-      .collection("products")
-      .doc("productsObject")
-      .get()
-      .then((info) => {
-        setProducts(info.data().products);
-        let newCategories = [];
-        for (var key in info.data().products) {
-          newCategories.push(key);
-        }
-        setCategories(newCategories);
-      });
-  }, []);
+	useEffect(() => {
+		firebase
+			.firestore()
+			.collection("products")
+			.doc("productsObject")
+			.get()
+			.then((info) => {
+				setProducts(info.data().products);
+				let newCategories = [];
+				for (var key in info.data().products) {
+					newCategories.push(key);
+				}
+				setCategories(newCategories);
+			});
+	}, []);
 
-  return (
-    <div className="main-page">
-      <div
-        style={{ backgroundImage: 'url("/images/biomart-is.png")' }}
-        className="photo-block"
-      >
-        <h1>
-          <span className="bold-text">BIOMART</span> - лучшее спортивное питание
+	return (
+		<div className="main-page">
+			<div
+				style={{ backgroundImage: 'url("/images/biomart-is.png")' }}
+				className="photo-block"
+			>
+				<h1>
+					<span className="bold-text">BIOMART</span> - лучшее спортивное питание
           для всей семьи
         </h1>
-      </div>
+			</div>
 
-      <div className="yellow-down-block">
-        <h1>Для постоянных посетителей существует система скидок ( до 15% )</h1>
-        <button>Купить сейчас</button>
-      </div>
+			<div className="yellow-down-block">
+				<h1>Для постоянных посетителей существует система скидок ( до 15% )</h1>
+				<button>Купить сейчас</button>
+			</div>
 
-      <div className="big-gray-block">
-        <h1 className="max-width">Сегодня в трендах:</h1>
-        <div className="white-boxes">
-          {categories.map((element) => {
-            return (
-              products[element].length > 0 &&
-              products[element].map((el, index) => {
-                counter += 1;
-                return (
-                  counter < 6 && (
-                    <div key={index} className="white-product-box">
-                      <div className="top-side">
-                        <div
-                          className="badge"
-                          style={{
-                            backgroundColor: hexObject[el.productBadge],
-                          }}
-                        >
-                          {el.productBadge}
-                        </div>
+			<div className="big-gray-block">
+				<h1 className="max-width">Сегодня в трендах:</h1>
+				<div className="white-boxes">
+					<HorizontalScroll reverseScroll={false} style={{
+						width: '1400px',
+						margin: '0 auto'
+					}}>
+						{categories.map((element) => {
+							return (
+								products[element].length > 0 &&
+								products[element].map((el, index) => {
+									counter += 1;
+									return (
+										counter < 6 && (
+											<div key={index} className="white-product-box">
+												<div className="top-side">
+													<div
+														className="badge"
+														style={{
+															backgroundColor: hexObject[el.productBadge],
+														}}
+													>
+														{el.productBadge}
+													</div>
 
-                        <Link to="/shop">
-                          <img
-                            src="/images/supermarket1.png"
-                            alt="supermarket"
-                          />
-                        </Link>
-                      </div>
-                      <img
-                        className="product-image"
-                        src={el.image}
-                        alt="protein"
-                      />
-                      <h1>{el.title}</h1>
-                      <h2>{el.price}p</h2>
-                    </div>
-                  )
-                );
-              })
-            );
-          })}
-        </div>
-      </div>
+													<Link to="/shop">
+														<img
+															src="/images/supermarket1.png"
+															alt="supermarket"
+														/>
+													</Link>
+												</div>
+												<img
+													className="product-image"
+													src={el.image}
+													alt="protein"
+												/>
+												<h1>{el.title}</h1>
+												<h2>{el.price}p</h2>
+											</div>
+										)
+									);
+								})
+							);
+						})}
+					</HorizontalScroll>
+				</div>
+			</div>
 
-      <p style={{ display: "none" }}>{(counter = 0)}</p>
+			<p style={{ display: "none" }}>{(counter = 0)}</p>
 
-      <div className="big-gray-block">
-        <h1 className="max-width">Рекомендуем:</h1>
-        <div className="white-boxes">
-          {categories.map((element) => {
-            return (
-              products[element].length > 0 &&
-              products[element].map((el, index) => {
-                counter += 1;
-                return (
-                  counter > 5 &&
-                  counter < 11 && (
-                    <div key={index} className="white-product-box">
-                      <div className="top-side">
-                        <div
-                          className="badge"
-                          style={{
-                            backgroundColor: hexObject[el.productBadge],
-                          }}
-                        >
-                          {el.productBadge}
-                        </div>
+			<div className="big-gray-block">
+				<h1 className="max-width">Рекомендуем:</h1>
+				<div className="white-boxes">
+					<HorizontalScroll reverseScroll={false} style={{
+						width: '1400px',
+						margin: '0 auto'
+					}}>
+						{categories.map((element) => {
+							return (
+								products[element].length > 0 &&
+								products[element].map((el, index) => {
+									counter += 1;
+									return (
+										counter > 5 &&
+										counter < 11 && (
+											<div key={index} className="white-product-box">
+												<div className="top-side">
+													<div
+														className="badge"
+														style={{
+															backgroundColor: hexObject[el.productBadge],
+														}}
+													>
+														{el.productBadge}
+													</div>
 
-                        <Link to="/shop">
-                          <img
-                            src="/images/supermarket1.png"
-                            alt="supermarket"
-                          />
-                        </Link>
-                      </div>
-                      <img
-                        className="product-image"
-                        src={el.image}
-                        alt="protein"
-                      />
-                      <h1>{el.title}</h1>
-                      <h2>{el.price}p</h2>
-                    </div>
-                  )
-                );
-              })
-            );
-          })}
-        </div>
-      </div>
+													<Link to="/shop">
+														<img
+															src="/images/supermarket1.png"
+															alt="supermarket"
+														/>
+													</Link>
+												</div>
+												<img
+													className="product-image"
+													src={el.image}
+													alt="protein"
+												/>
+												<h1>{el.title}</h1>
+												<h2>{el.price}p</h2>
+											</div>
+										)
+									);
+								})
+							);
+						})}
+					</HorizontalScroll>
+				</div>
+			</div>
 
-      <div className="green-block">
-        <h1>
-          Для постоянных покупателей магази будет задействована система скидок.
+			<div className="green-block">
+				<h1>
+					Для постоянных покупателей магази будет задействована система скидок.
         </h1>
-      </div>
-      <Footer />
-    </div>
-  );
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default NewMainPage;
