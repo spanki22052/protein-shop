e5e806635d0fd874a72387d3d1faddbbed13e9c7
spanki22052 <useful22052@gmail.com>
2020-09-20 22:31:46 +0300

    add functional for product page

--- protein/src/components/ProductPage/index.jsx
+++ protein/src/components/ProductPage/index.jsx
@@ -2,42 +2,150 @@ import React, { useState, useEffect } from "react";
 import "./product.scss";
 import { connect } from "react-redux";
 
-const ProductPage = ({ shopStore }) => {
-	const [currentProduct, setProd] = useState([]);
-	const currentUrl = window.location.href.split("/")[
-		window.location.href.split("/").length - 1
-	];
+const ProductPage = ({
+  shopStore,
+  sendProduct,
+  prods,
+  items,
+  sendProductToList,
+}) => {
+  const [currentProduct, setProd] = useState([]);
+  const [amountInput, setAmount] = useState("1");
+  const currentUrl = window.location.href.split("/")[
+    window.location.href.split("/").length - 1
+  ];
 
-	useEffect(() => {
-		shopStore.products.map(
-			(el) => el.productEngl === currentUrl && setProd(el)
-		);
-	});
+  useEffect(() => {
+    shopStore.products.map(
+      (el) => el.productEngl === currentUrl && setProd(el)
+    );
+  });
 
-	return (
-		<>
-			{/* <div className="product-page">
+  const hexObject = {
+    "В НАЛИЧИИ": "white",
+    "СКИДКА 10%": "#EC2929",
+    ПРЕДЗАКАЗ: "#265ECB",
+    "НЕТ В НАЛИЧИИ": "#BCBCBC",
+  };
+
+  const addNewProduct = (title, price, image) => {
+    var payload = {};
+    prods.hasOwnProperty(title) === false
+      ? (payload = {
+          title: title,
+          price: price,
+          image: image,
+          amount: 1,
+        })
+      : (payload = {
+          title: title,
+          price: price,
+          image: image,
+          amount: prods[title].amount + 1,
+        });
+    sendProduct(payload);
+    items.includes(title) === false && sendProductToList(title);
+  };
+
+  return (
+    <>
+      {/* <div className="product-page">
 				<h1>{currentProduct.title}</h1>
 				<h2>{currentProduct.productDescription}</h2>
 			</div> */}
-			<div className='product-page'>
-				<img src='/photo.jpg' className='product-photo' />
-				<div className='product-info'>
-					<div className='product-title'>{currentProduct.title}</div>
-					<div className='product-suptitle'>{currentProduct.productDescription}Сывороточный протеин № 1 в Великобритании — отличное дополнение к вашему ежедневному рациону</div>
-					<div className='product-price'><span>Скидка</span> {currentProduct.price} руб</div>
-					<div className='product-quantity'>
-						<button className='add-product'>+</button>
-						<input className='product-number'></input>
-						<button className='remove-product'>-</button>
-					</div>
-					<div className='product-add'>Добавить в корзину</div>
-				</div>
-			</div>
-		</>
-	);
+      <div className="product-page">
+        <div className="image-holder">
+          <div
+            className="badge"
+            style={{
+              backgroundColor: hexObject[currentProduct.productBadge],
+            }}
+          >
+            {currentProduct.productBadge}
+          </div>
+          <img
+            src={currentProduct.image}
+            className="product-photo"
+            alt="productImage"
+          ></img>
+        </div>
+
+        <div className="product-info">
+          <div className="product-title">{currentProduct.title}</div>
+          <div className="product-suptitle">
+            {currentProduct.productDescription}Сывороточный протеин № 1 в
+            Великобритании — отличное дополнение к вашему ежедневному рациону
+          </div>
+          <div className="product-price">
+            {currentProduct.productBadge === "СКИДКА 10%" && (
+              <span>
+                {parseInt(currentProduct.price) +
+                  parseInt(currentProduct.price) * 0.1}
+              </span>
+            )}
+            {currentProduct.price}
+            руб
+          </div>
+          <div className="product-quantity">
+            <button
+              className="remove-product"
+              onClick={() => {
+                parseInt(amountInput) > 1 &&
+                  setAmount(parseInt(amountInput) - 1);
+              }}
+            >
+              -
+            </button>
+            <input
+              className="product-number"
+              value={amountInput}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+            <button
+              className="add-product"
+              onClick={() => {
+                setAmount(parseInt(amountInput) + 1);
+              }}
+            >
+              +
+            </button>
+          </div>
+          <div
+            className="product-add"
+            onClick={() =>
+              addNewProduct(
+                currentProduct.title,
+                currentProduct.price,
+                currentProduct.image
+              )
+            }
+          >
+            Добавить в корзину
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default connect((state) => ({
-	shopStore: state.shopStore,
-}))(ProductPage);
+export default connect(
+  (state) => ({
+    shopStore: state.shopStore,
+    prods: state.addItem,
+    items: state.returnItems,
+  }),
+  (dispatch) => ({
+    sendProduct: (payload) => {
+      dispatch({
+        type: "ADD_PRODUCT",
+        payload,
+      });
+    },
+    sendProductToList: (payload) => {
+      dispatch({
+        type: "ADD_ITEM",
+        payload,
+      });
+    },
+  })
+)(ProductPage);
